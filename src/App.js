53b68de856c535606da51dd473eb3d9579b18281
Erick Pacheco Pedraza <eum602@gmail.com>
2019-04-20 18:21:05 -0500

    modified App.js from master to rebase another branch

--- src/App.js
+++ src/App.js
@@ -50,6 +50,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+	Adding some comments from master
         <Dogs dogs={this.state.dogs} onDelete={this.handleDeleteDog.bind(this)} />
         <AddDog addDog={this.handleAddDog.bind(this)} />
         <hr />
